1971f4c685cfd6f265fb00fd4cff55e7f1cf74d9
amubawrnaw <amubawrnaw@gmail.com>
2018-07-30 22:25:09 +0800

    added skeleton classes

---
+++ Model/Station.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Station{
+	ArrayList<Train> trains;
+	ArrayList<Passenger> passengers;
+
+	public Station(){
+
+	}
+
+	public void station_load_train(int count){
+		//TODO this function
+	}
+
+	public void station_wait_for_train(){
+		//TODO this function
+	}
+
+	public void station_on_board(){
+		//TODO this function?
+	}
+
+}
---
+++ Model/Train.java
@@ -0,0 +1,9 @@
+
+public class Train{
+
+	public Train(){
+		
+	}
+
+
+}
